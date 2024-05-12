2015c851927aaf73abbdaf5065cb56a7f79a6659
shixiongfei <xiongfei.shi@icloud.com>
2024-05-12 12:25:15 +0800

    Simplify interfaces

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,24 +9,67 @@
  * https://github.com/shixiongfei/integer-ts
  */
 
-import Integer, { integer } from "./index.js";
-
-console.log(Integer.of(NaN));
-console.log(Integer.of(Infinity));
-console.log(Integer.of(-Infinity));
-console.log(Integer.of(123));
-console.log(Integer.of(3.14));
-console.log(Integer.of((0.1 + 0.2) * 10));
-console.log(Integer.of((0.1 + 0.2) * -10));
-console.log(Integer.of((0.1 + 0.7) * 10));
-console.log(Integer.of((-0.1 - 0.7) * 10));
-console.log(Integer.of(12) + 3.14);
-console.log((123.456).toInteger() + 241);
-console.log(Number.ofInteger(345.789) + 456);
+import assert from "node:assert";
+import test from "node:test";
+import int, { Integer, integer } from "./index.js";
 
 const add = (a: integer, b: integer) => a + b;
 
+// add(2, 3)
 // error TS2345: Argument of type 'number' is not assignable to parameter of type 'integer'.
-// console.log(add(2, 3));
 
-console.log(add(Integer.of(2), Integer.of(3)));
+test("that int(NaN) is equal 0", () => {
+  assert.strictEqual(int(NaN), 0);
+});
+
+test("that int(Infinity) is equal Number.MAX_SAFE_INTEGER", () => {
+  assert.strictEqual(int(Infinity), Number.MAX_SAFE_INTEGER);
+});
+
+test("that int(-Infinity) is equal Number.MIN_SAFE_INTEGER", () => {
+  assert.strictEqual(int(-Infinity), Number.MIN_SAFE_INTEGER);
+});
+
+test("that int(123) is equal 123", () => {
+  assert.strictEqual(int(123), 123);
+});
+
+test("that int(3.14) is equal 3", () => {
+  assert.strictEqual(int(3.14), 3);
+});
+
+test("that int((0.1 + 0.2) * 10) is equal 3", () => {
+  assert.strictEqual(int((0.1 + 0.2) * 10), 3);
+});
+
+test("that int((0.1 + 0.2) * -10) is equal -3", () => {
+  assert.strictEqual(int((0.1 + 0.2) * -10), -3);
+});
+
+test("that int((0.1 + 0.7) * 10) is equal 8", () => {
+  assert.strictEqual(int((0.1 + 0.7) * 10), 8);
+});
+
+test("that int((-0.1 - 0.7) * 10) is equal -8", () => {
+  assert.strictEqual(int((-0.1 - 0.7) * 10), -8);
+});
+
+test("that int(12) + 3.14 is equal 15.14", () => {
+  assert.strictEqual(int(12) + 3.14, 15.14);
+});
+
+test("that (123.456).toInteger() + 241 is equal 364", () => {
+  assert.strictEqual((123.456).toInteger() + 241, 364);
+});
+
+test("that (345.678 + 456.789).toInteger() is equal 802", () => {
+  assert.strictEqual((345.678 + 456.789).toInteger(), 802);
+});
+
+test("that add(Integer(2), Integer(3)) is equal 5", () => {
+  assert.strictEqual(add(Integer(2), Integer(3)), 5);
+});
+
+test("that Integer(12.34) + Integer(34.56) is equal 46", () => {
+  assert.strictEqual(Integer(12.34) + Integer(34.56), 46);
+});
--- src/index.ts
+++ src/index.ts
@@ -12,44 +12,34 @@
 const __integer_type__ = Symbol();
 export type integer = number & { readonly [__integer_type__]: unique symbol };
 
-export class Integer {
-  static of(value: number): integer {
-    if (Number.isInteger(value)) {
-      return value as integer;
-    }
-
-    if (Number.isFinite(value)) {
-      return Math.floor(Math.round(value * 10) / 10) as integer;
-    }
-
-    if (value === Number.POSITIVE_INFINITY) {
-      return Number.MAX_SAFE_INTEGER as integer;
-    }
-
-    if (value === Number.NEGATIVE_INFINITY) {
-      return Number.MIN_SAFE_INTEGER as integer;
-    }
-
-    return 0 as integer;
+export const Integer = function (value: number): integer {
+  if (Number.isInteger(value)) {
+    return value as integer;
   }
-}
+
+  if (Number.isFinite(value)) {
+    return Math.floor(Math.round(value * 10) / 10) as integer;
+  }
+
+  if (value === Number.POSITIVE_INFINITY) {
+    return Number.MAX_SAFE_INTEGER as integer;
+  }
+
+  if (value === Number.NEGATIVE_INFINITY) {
+    return Number.MIN_SAFE_INTEGER as integer;
+  }
+
+  return 0 as integer;
+};
 
 declare global {
-  export interface NumberConstructor {
-    ofInteger(value: number): integer;
-  }
-
   export interface Number {
     toInteger: () => integer;
   }
 }
 
-Number.ofInteger = function (value: number) {
-  return Integer.of(value);
-};
-
 Number.prototype.toInteger = function (this: number) {
-  return Integer.of(this);
+  return Integer(this);
 };
 
 export default Integer;
